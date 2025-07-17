01aea9a55d4e63c0c583a1bd12f001180f00a732
ast_vkapitan <i.kapitan@astoundcommerce.com>
2025-07-17 12:04:34 +0300

    Learning JS. Vue3 bootcamp. App6 - AnimationApp: simple fade animation

--- udemy/vue3-bootcamp/AnimationApp/src/views/HomeView.vue
+++ udemy/vue3-bootcamp/AnimationApp/src/views/HomeView.vue
@@ -1,10 +1,42 @@
 <script setup>
+  import { ref, Transition } from 'vue';
+
+  const showText = ref (false);
 </script>
 
 <template>
   <main>
+    <Transition name="fade">
+      <h1 v-if="showText">Hello World</h1>
+    </Transition>
+
+    <button @click="showText=!showText">Toggle</button>
   </main>
 </template>
 
 <style scoped>
+  .fade-enter-from {
+    opacity: 0;
+  }
+
+  .fade-enter-to {
+    opacity: 1;
+  }
+
+  .fade-enter-active {
+    transition: all 1s ease;
+  }
+
+  
+  .fade-leave-from {
+    opacity: 1;
+  }
+
+  .fade-leave-to {
+    opacity: 0;
+  }
+
+  .fade-leave-active {
+    transition: all 1s ease-in-out;
+  }
 </style>
