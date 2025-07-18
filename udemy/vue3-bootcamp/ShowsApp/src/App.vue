138e170e059b19d84271a12edbeeed4c71b3a047
ast_vkapitan <i.kapitan@astoundcommerce.com>
2025-07-18 11:46:06 +0300

    Learning JS. Vue3 bootcamp. App7 - ShowsApp. Last AirbenderCards

--- udemy/vue3-bootcamp/ShowsApp/src/App.vue
+++ udemy/vue3-bootcamp/ShowsApp/src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+  import LastAirbenderCards from './components/LastAirbenderCards.vue';
 </script>
 
 <template>
@@ -6,6 +7,15 @@
   </header>
 
   <main>
+    <h1>Hero</h1>
+    <Suspense>
+      <template #default>
+        <LastAirbenderCards />
+      </template>
+      <template #fallback>
+        <div>Loading...</div>
+      </template>
+    </Suspense>
   </main>
 </template>
 
---
+++ udemy/vue3-bootcamp/ShowsApp/src/components/Card.vue
@@ -0,0 +1,42 @@
+<script setup>
+  import {defineProps} from 'vue';
+
+  const {image, name, affiliation, allies} = defineProps([ 'image', 'name', 'affiliation', 'allies' ]);
+</script>
+
+<template>
+  <n-card :title="name">
+    <template #cover>
+      <img :src="image">
+    </template>
+    <h3>{{ affiliation }}</h3>
+    <div class="jobs">
+      <p v-for="(ally, index) in allies" :key="ally">
+        {{ ally }}
+        <span v-if="index < allies.length-1">,&nbsp;</span>
+      </p>
+    </div>
+  </n-card>
+</template>
+
+<style scoped>
+
+.n-card {
+    max-width: 300px;
+    width: 300px;
+    margin:10px 20px;
+
+}
+.n-card img {
+    height: 250px
+}
+
+p {
+    font-size: 10px;
+}
+
+.jobs {
+    display: flex;
+    flex-wrap: wrap;
+}
+</style>
