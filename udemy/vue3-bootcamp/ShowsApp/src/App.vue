5dd76ea64dfe8a3f57b6827bf40c8e2d108f870c
ast_vkapitan <i.kapitan@astoundcommerce.com>
2025-07-20 14:57:26 +0300

    Learning JS. Vue3 bootcamp. App7 - ShowsApp. Add Header for Shows app

--- udemy/vue3-bootcamp/ShowsApp/src/App.vue
+++ udemy/vue3-bootcamp/ShowsApp/src/App.vue
@@ -1,41 +1,25 @@
 <script setup>
-  import LastAirbenderCards from './components/LastAirbenderCards.vue';
+  import { ref } from 'vue';
+  import LastAirbenderCardsSuspense from './components/LastAirbenderCardsSuspense.vue';
   import RickAndMortyCards from './components/RickAndMortyCards.vue';
+  import Hero from './components/Hero.vue';
+
+  const isLastAirbender = ref(true);
 </script>
 
 <template>
   <header>
+    <Hero
+      :isLastAirbender="isLastAirbender"
+      @selectShow="isLastAirbender = !isLastAirbender"
+    />
   </header>
 
   <main>
-    <h1>Hero</h1>
-    <Suspense>
-      <template #default>
-        <LastAirbenderCards />
-      </template>
-      <template #fallback >
-        <div class="cards spinner">
-          <div>
-            <div>Loading...</div>
-            <n-spin size="large" />
-          </div>
-        </div>
-      </template>
-    </Suspense>
-    <RickAndMortyCards />
+    <LastAirbenderCardsSuspense v-if="isLastAirbender"/>
+    <RickAndMortyCards v-else/>
   </main>
 </template>
 
 <style scoped>
-  .cards {
-    height: 400px;
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-
-    .n-spin-body {
-      width: 100%;
-    }
-  }
 </style>
---
+++ udemy/vue3-bootcamp/ShowsApp/src/components/Hero.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import {ref, watch, defineProps, defineEmits} from 'vue';
+
+  const emit = defineEmits(['selectShow']);
+  const {isLastAirbender} = defineProps(['isLastAirbender']);
+
+  const emitSelectedShow = () => emit('selectShow');
+</script>
+
+<template>
+  <div class="hero">
+    <h1>{{ isLastAirbender ? 'Last Airbender' : 'Rick and Morty' }}</h1>
+    <p @click="emitSelectedShow">{{ !isLastAirbender ? 'Last Airbender' : 'Rick and Morty' }}</p>
+  </div>
+</template>
+
+<style scoped>
+.hero {
+    height : 40vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    flex-direction: column
+}
+.hero img {
+    position: absolute;
+    height: 700px;
+    opacity: 0.1;
+}
+.hero h1 {
+    font-size: 100px;
+    font-weight: bold;
+}
+.hero p {
+    cursor: pointer;
+}
+</style>
