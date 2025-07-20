6912293efe68ba98051596e13c0ba5564659ea65
ast_vkapitan <i.kapitan@astoundcommerce.com>
2025-07-21 00:31:30 +0300

    Learning JS. Vue3 bootcamp. App9 - TypeScriptApp. Init

---
+++ udemy/vue3-bootcamp/TypeScriptApp/src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const number = ref('');
+</script>
+
+<template>
+  <main>
+    <h1>Guess my Favorite Number</h1>
+    <input type="text" v-model="number">
+    <h2 v-if="number === '27'">You Guess it</h2>
+  </main>
+</template>
+
+<style scoped>
+</style>
