bd54d79b7f5470a8d61c3160e27a17943b7c798d
ast_vkapitan <i.kapitan@astoundcommerce.com>
2025-07-27 00:56:28 +0300

    Learning JS. Vue3 bootcamp. App10 - InstagramApp. Hide sensitive data

--- udemy/vue3-bootcamp/InstagramApp/src/superbase.js
+++ udemy/vue3-bootcamp/InstagramApp/src/superbase.js
@@ -1,6 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://xuayslhplmadovjgvhkv.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1YXlzbGhwbG1hZG92amd2aGt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMxMjU0MzcsImV4cCI6MjA2ODcwMTQzN30.Xbzy3TAPS-KNWmlO4AUTM6wX56yEN6cwbXCMX3AUSn8'// process.env.SUPABASE_KEY
+console.log('supabaseUrl', import.meta.env.VITE_SUPABASE_URL);
+console.log('supabaseKey', import.meta.env.VITE_SUPABASE_KEY);
 
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
+)
