6c5375ccf3b9b933201867016c2ed9e98944f5b0
ast_vkapitan <i.kapitan@astoundcommerce.com>
2025-07-27 00:57:54 +0300

    Learning JS. Vue3 bootcamp. App10 - InstagramApp. Hide sensitive data

--- udemy/vue3-bootcamp/InstagramApp/src/superbase.js
+++ udemy/vue3-bootcamp/InstagramApp/src/superbase.js
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log('supabaseUrl', import.meta.env.VITE_SUPABASE_URL);
-console.log('supabaseKey', import.meta.env.VITE_SUPABASE_KEY);
-
 export const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
   import.meta.env.VITE_SUPABASE_KEY
