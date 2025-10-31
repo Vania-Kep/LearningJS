41d5c2c4b01fb2640335a17eefc5321351ca643a
ast_vkapitan <i.kapitan@astoundcommerce.com>
2025-10-31 12:43:57 +0200

    Learning JS. Udemy. React. React - Complete Guide 2025. Styling React Components. Styling - use "tailwind CSS" package (lesson 124)

--- udemy/React/reactJS-theCompleteGuide2025/reactJS__styling/src/components/UI/Button.jsx
+++ udemy/React/reactJS-theCompleteGuide2025/reactJS__styling/src/components/UI/Button.jsx
@@ -1,18 +1,27 @@
-import { styled } from 'styled-components';
+// import { styled } from 'styled-components';
 
-const Button = styled.button`
-  padding: 1rem 2rem;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-radius: 0.25rem;
-  color: #1f2937;
-  background-color: #f0b322;
-  border-radius: 6px;
-  border: none;
+// const Button = styled.button`
+//   padding: 1rem 2rem;
+//   font-weight: 600;
+//   text-transform: uppercase;
+//   border-radius: 0.25rem;
+//   color: #1f2937;
+//   background-color: #f0b322;
+//   border-radius: 6px;
+//   border: none;
 
-  &:hover {
-    background-color: #f0920e;
-  }
-`;
+//   &:hover {
+//     background-color: #f0920e;
+//   }
+// `;
+
+//tailwindcss version
+function Button({ children, ...props }) {
+  return (
+    <button {...props} className="px-4 py-2 font-semibold uppercase rounded text-gray-900 bg-amber-400 hover:bg-amber-500">
+      {children}
+    </button>
+  )
+}
 
 export default Button
